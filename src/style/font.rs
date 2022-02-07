34f22c23fd5c7e798535146efb365f18e897f9cf
Teddy Byron <ted@tedbyron.com>
2022-02-07 01:06:01 -0500

    update ui

---
+++ src/style/font.rs
@@ -0,0 +1,23 @@
+//! External fonts.
+
+use iced::Font;
+
+pub const IOSEVKA: Font = Font::External {
+    name: "Iosevka",
+    bytes: include_bytes!("../../assets/fonts/iosevka-ss06-extended.ttf"),
+};
+
+pub const _IOSEVKA_BOLD: Font = Font::External {
+    name: "Iosevka Bold",
+    bytes: include_bytes!("../../assets/fonts/iosevka-ss06-extendedbold.ttf"),
+};
+
+pub const IOSEVKA_HEAVY_ITALIC: Font = Font::External {
+    name: "Iosevka Heavy Italic",
+    bytes: include_bytes!("../../assets/fonts/iosevka-ss06-extendedheavyitalic.ttf"),
+};
+
+pub const _IOSEVKA_LIGHT_ITALIC: Font = Font::External {
+    name: "Iosevka Light Italic",
+    bytes: include_bytes!("../../assets/fonts/iosevka-ss06-extendedlightitalic.ttf"),
+};
---
+++ src/view/mod.rs
@@ -0,0 +1,5 @@
+//! UI views and elements.
+
+pub mod edit_file;
+pub mod notification;
+pub mod select_file;
