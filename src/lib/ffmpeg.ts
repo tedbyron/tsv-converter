94832749005c81477a0aa96002b0132e30e9d4b1
Teddy Byron <ted@tedbyron.com>
2022-04-08 18:45:37 -0400

    add loading icon

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,5 +1,6 @@
 import { Command } from '@tauri-apps/api/shell'
 
+/** Whether or not `ffprobe` can read the file's metadata. */
 export const isVideo = async (path: string): Promise<boolean> => {
   const { code } = await Command.sidecar('bin/ffprobe', path).execute()
   return code === 0
